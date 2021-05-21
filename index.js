39fbf8a5216172a07f29145490bfc108ed3f5533
reetah589 <reetah589@gmail.com>
2021-05-20 19:10:35 -0500

    corrected writetofile

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // TODO: Include packages needed for this application
 var inquirer = require('inquirer');
-const fs = require("fs")
+const fs = require("fs");
+const { title } = require('process');
 
 // TODO: Create an array of questions for user input
 var questions = [{
@@ -19,19 +20,25 @@ var questions = [{
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {
-  return fs.writeFileSync(path.join(process.cwd(), fileName), data); 
+  return fs.writeFileSync(fileName, data); 
 }
 
 
 
 inquirer
-  .prompt([
-    /* Pass your questions in here */
-  ])
+ /* Pass your questions in here */
+  .prompt(questions)
   .then(answers => {
     // Use user feedback for... whatever!!
+    console.log(answers)
+    console.log(answers.title)
+    console.log(answers.description)
+    // use the writeToFile function here
+    writeToFile("Readme-Test.md", answers.title + "\n" + answers.description
+    )
   })
   .catch(error => {
+    console.log(error)
     if(error.isTtyError) {
       // Prompt couldn't be rendered in the current environment
     } else {
